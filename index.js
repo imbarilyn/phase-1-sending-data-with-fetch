0987cd29297223eaf6f1a453dd5833efc9b897a6
imbarilyn <linahmuhonjaimbari@gmail.com>
2022-08-25 06:37:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json"
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(resp => resp.json())
+// .then(dog => console.log(dog))
+const para = document.createElement("p");
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    })
+    .then(resp => resp.json())
+    .then((user) => {
+        console.log(user);
+        para.textContent = user.id;
+        document.body.appendChild(para);
+    })
+    .catch(e =>{
+        alert("Error!!");
+        para.textContent = e.message;
+        document.body.appendChild(para);
+    })
+} 
+
+submitData("Steve", "steve@steve.com"); 
+
+
+
+
